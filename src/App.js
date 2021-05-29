1f13d9adecab644b6c316d5f9478ca74ab6fdba9
theharshnigam <er.harshnigam@gmail.com>
2021-05-29 17:59:18 +0530

    before displaying moves and applying css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import Board from './components/Board';
+import calculateWinner from "./Helpers";
 import './styles/styles.scss'
 
 
 const App = () => {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+  console.log(winner);
+
+  const message = winner ? `Winner is ${winner}` : `Next Player is ${isXNext ? 'X' : 'O'}`;
+
+  
+
+  const handleSquareClick = (position) => {
+    // If already clicked on a square, value will not be changed.
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard((prev) => {
+      return prev.map((square, pos) => {
+
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+
+    setIsXNext((prev) => !prev);
+  };
+
+
   return (
-    <div className = "app">
+    <div className="app">
       <h1>Tic Tac Toe</h1>
-      <Board/>
+      <h2>{message}</h2>
+      <Board board = {board} handleSquareClick = {handleSquareClick} />
+      <button type = "button" >Start New Game</button>
     </div>
-  );
+  )
 };
 
 export default App;
